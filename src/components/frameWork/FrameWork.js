8f5b321a79c7bb5e16f5c1bc3ac26589e97c1471
Md. Shahadat Hossen <mhossen171243@bscse.uiu.ac.bd>
2021-12-25 21:54:29 +0600

    new update

--- src/components/frameWork/FrameWork.js
+++ src/components/frameWork/FrameWork.js
@@ -11,49 +11,42 @@ class FrameWork extends Component {
                 <Container fluid={true}>
                     <Row>
                         <Col lg={8} md={8} sm={12} className='colSideBar topMargin'>
-                        <h3 className='text-center title'>I WANT MY SITE TO BE BUILT ON (CHOOSE FRAMEWORK)</h3>
-                            {/* <Row> */}
-                                {/* <Col lg={3} md={3} sm={0}></Col> */}
-                                {/* <Col> */}
-                                    <Card className='card'>
-                                        <Card.Body>
-                                            <Card.Title className='cardTitle'>Python</Card.Title>
-                                            <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</Card.Text>
-                                        </Card.Body>
-                                    </Card>
-                                    <Card className='card'>
-                                        <Card.Body>
-                                            <Card.Title className='cardTitle'>Wordpress</Card.Title>
-                                            <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</Card.Text>
-                                        </Card.Body>
-                                    </Card>
-                                    <Card className='card'>
-                                        <Card.Body>
-                                            <Card.Title className='cardTitle'>Php Laravel</Card.Title>
-                                            <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</Card.Text>
-                                        </Card.Body>
-                                    </Card>
-                                    <Card className='card'>
-                                        <Card.Body>
-                                            <Card.Title className='cardTitle'>Php (From Scratch)</Card.Title>
-                                            <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</Card.Text>
-                                        </Card.Body>
-                                    </Card>
-                                    <Card className='card'>
-                                        <Card.Body>
-                                            <Card.Title className='cardTitle'>Kajabi</Card.Title>
-                                            <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</Card.Text>
-                                        </Card.Body>
-                                    </Card>
-                                    <div className='prevNextDiv'>
-                                        <Button className='disableButton previous' disabled={true}>PREVIOUS</Button>
-                                        <ProgressBar className='progressBar' now={33} />
-                                        <Button className='enableButton next'><Link className='enableButtonLink' to="/webtype">NEXT</Link></Button>
-                                    </div>
-                                {/* </Col> */}
-                                {/* <Col lg={3} md={3} sm={0}></Col> */}
-                            {/* </Row> */}
-                            
+                            <h3 className='text-center title'>I WANT MY SITE TO BE BUILT ON (CHOOSE FRAMEWORK)</h3>
+                            <Card className='card'>
+                                <Card.Body>
+                                    <Card.Title className='cardTitle'>Python</Card.Title>
+                                    <Card.Text className='carddetails'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</Card.Text>
+                                </Card.Body>
+                            </Card>
+                            <Card className='card'>
+                                <Card.Body>
+                                    <Card.Title className='cardTitle'>Wordpress</Card.Title>
+                                    <Card.Text className='carddetails'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</Card.Text>
+                                </Card.Body>
+                            </Card>
+                            <Card className='card'>
+                                <Card.Body>
+                                    <Card.Title className='cardTitle'>Php Laravel</Card.Title>
+                                    <Card.Text className='carddetails'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</Card.Text>
+                                </Card.Body>
+                            </Card>
+                            <Card className='card'>
+                                <Card.Body>
+                                    <Card.Title className='cardTitle'>Php (From Scratch)</Card.Title>
+                                    <Card.Text className='carddetails'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</Card.Text>
+                                </Card.Body>
+                            </Card>
+                            <Card className='card'>
+                                <Card.Body>
+                                    <Card.Title className='cardTitle'>Kajabi</Card.Title>
+                                    <Card.Text className='carddetails'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</Card.Text>
+                                </Card.Body>
+                            </Card>
+                            <div className='prevNextDiv'>
+                                <Button className='disableButton previous' disabled={true}>PREVIOUS</Button>
+                                <ProgressBar className='progressBar' now={33} />
+                                <Button className='enableButton next'><Link className='enableButtonLink' to="/webtype">NEXT</Link></Button>
+                            </div>
                         </Col>
                         <Col lg={4} md={4} sm={12} className='topMargin'>
                             <h5 className='rightHeading'>PLEASE INPUT ALL THE FIELDS TO<br/>SHOW ESTIMATED PRICE</h5>
--- src/components/webtype/WebType.js
+++ src/components/webtype/WebType.js
@@ -10,48 +10,40 @@ class WebType extends Component {
             <Fragment>
                 <Container fluid={true}>
                     <Row>
-                        <Col lg={8} md={8} sm={12}>
-                        <h3 className='text-center'>I WANT TO BUILD</h3>
-                            <Row>
-                                <Col lg={2} md={2} sm={0}></Col>
-                                <Col lg={8} md={8} sm={12}>
-                                    <Card>
-                                        <Card.Body>
-                                            <Card.Title>An ecommerce site</Card.Title>
-                                            <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </Card.Text>
-                                        </Card.Body>
-                                    </Card>
-                                    <Card>
-                                        <Card.Body>
-                                            <Card.Title>An online course platform</Card.Title>
-                                            <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </Card.Text>
-                                        </Card.Body>
-                                    </Card>
-                                    <Card>
-                                        <Card.Body>
-                                            <Card.Title>A blog site</Card.Title>
-                                            <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </Card.Text>
-                                        </Card.Body>
-                                    </Card>
-                                    <Card>
-                                        <Card.Body>
-                                            <Card.Title>My company website with pics calculations</Card.Title>
-                                            <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </Card.Text>
-                                        </Card.Body>
-                                    </Card>
-                                    <div>
-                                        
-                                        <Button><Link to="/">PREVIOUS</Link></Button>
-                                        <ProgressBar now={66} />
-                                        <Button><Link to="/details">NEXT</Link></Button>
-                                    </div>
-                                </Col>
-                                <Col lg={2} md={2} sm={0}></Col>
-                            </Row>
-                            
+                        <Col lg={8} md={8} sm={12} className='colSideBar topMargin'>
+                            <h3 className='text-center title'>I WANT TO BUILD</h3>
+                            <Card className='card'>
+                                <Card.Body>
+                                    <Card.Title className='cardTitle'>An ecommerce site</Card.Title>
+                                    <Card.Text className='carddetails'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</Card.Text>
+                                </Card.Body>
+                            </Card>
+                            <Card className='card'>
+                                <Card.Body>
+                                    <Card.Title className='cardTitle'>An online course platform</Card.Title>
+                                    <Card.Text className='carddetails'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</Card.Text>
+                                </Card.Body>
+                            </Card>
+                            <Card className='card'>
+                                <Card.Body>
+                                    <Card.Title className='cardTitle'>A blog site</Card.Title>
+                                    <Card.Text className='carddetails'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</Card.Text>
+                                </Card.Body>
+                            </Card>
+                            <Card className='card'>
+                                <Card.Body>
+                                    <Card.Title className='cardTitle'>My company website with pics calculations</Card.Title>
+                                    <Card.Text className='carddetails'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</Card.Text>
+                                </Card.Body>
+                            </Card>
+                            <div className='prevNextDiv'>            
+                               <Button className='enableButton previous'><Link className='enableButtonLink' to="/">PREVIOUS</Link></Button>
+                                <ProgressBar className='progressBar' now={66} />
+                                <Button className='enableButton next'><Link className='enableButtonLink' to="/details">NEXT</Link></Button>
+                            </div>                            
                         </Col>
-                        <Col lg={4} md={4} sm={12}>
-                            <h5>PLEASE INPUT ALL THE FIELDS TO<br/>SHOW ESTIMATED PRICE</h5>
+                        <Col lg={4} md={4} sm={12} className='topMargin'>
+                            <h5 className='rightHeading'>PLEASE INPUT ALL THE FIELDS TO<br/>SHOW ESTIMATED PRICE</h5>
                         </Col>
                     </Row>
                 </Container>
